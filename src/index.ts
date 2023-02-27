94f32faa69e4a9b52118576eaa097df6771d192a
prazdevs <1631886+prazdevs@users.noreply.github.com>
2023-02-28 00:32:14 +0100

    feat: no prefix

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,21 @@ import { type Preset, type PresetOptions } from 'unocss'
 import { variants } from '@catppuccin/palette'
 
 export interface PresetCatppuccinOptions extends PresetOptions {
+  /**
+   * Adds the colours from this flavour accessible from `-ctp-color` directly.
+   */
   defaultFlavour?: keyof typeof variants
-  prefix?: string
+
+  /**
+   * Override default color prefix. Use `false` to remove it completely.
+   */
+  prefix?: string | false
 }
 
+/**
+ * UnoCSS preset to extend theme with Catppuccin colours.
+ * @param options preset options
+ */
 export const presetCatppuccin = (options: PresetCatppuccinOptions = {}): Preset => {
   const {
     defaultFlavour,
@@ -30,12 +41,9 @@ export const presetCatppuccin = (options: PresetCatppuccinOptions = {}): Preset 
   return {
     name: 'unocss-preset-catppuccin',
     theme: {
-      colors: {
-        [prefix]: {
-          ...flavoured,
-          ...palette,
-        },
-      },
+      colors: prefix 
+        ? { [prefix]: { ...flavoured, ...palette } }
+        : { ...flavoured, ...palette }
     },
   }
 }
